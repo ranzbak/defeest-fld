95bb70013ee5bf438a07a39ca9e589ad5ec95ed3
Paul Honig <paul@etv.cx>
2017-08-22 23:24:01 +0200

    Added a scroller to the screen, next up color animations.

--- badline.asm
+++ badline.asm
@@ -24,10 +24,15 @@ BasicUpstart2(begin)				// <- This creates a basic sys line that can start your 
 .const REG_BGCOLOUR        = $d021              // background colour register
 .const REG_INTSTATUS_1     = $dc0d              // interrupt control and status register #1
 .const REG_INTSTATUS_2     = $dd0d              // interrupt control and status register #2
+.const REG_ZERO_FE         = $fe								// Zero page cache
+.const REG_ZERO_FD         = $fd								// Zero page cache
 
 // constants
-.const C_SCREEN_RAM = $0400              // screen RAM
-.const C_COLOUR_RAM = $d800              // colour ram
+.const C_SCREEN_RAM				 = $0400              // screen RAM
+.const C_COLOUR_RAM				 = $d800              // colour ram
+.const C_CHARSET           = $3800							// Alternate Character bank
+.const C_CHARSET_HIGH			 = $3900							// Arternate Character bank HI
+
 
 // Koala resource
 .var picture = LoadBinary("defeest-fullscreen.kla", BF_KOALA)
@@ -55,7 +60,8 @@ begin:
 	stx REG_INTSERVICE_HIGH
 
 	// Initial screen memory setup
-	lda #%00010110								  // $1800-1ffff char mem, $0400-07ff screen mem
+	//lda #%00010110								  // $1800-1ffff char mem, $0400-07ff screen mem
+	lda #%00011010								  // $2800-1ffff char mem, $0400-07ff screen mem
 	sta REG_MEMSETUP
 
 	// Setup background sprites
@@ -170,7 +176,7 @@ sync_intro:
 	loop1:
 	.for (var i=0; i<4; i++) {
 		lda colorRam+i*$100,x
-		sta $d800+i*$100,x
+		sta C_COLOUR_RAM+i*$100,x
 	}
 	inx
 	bne loop1
@@ -178,15 +184,23 @@ sync_intro:
 	lda #$ff                        // init video garbage
 	sta $3fff                       // fill to highlight fld for debugging
 
+	jsr setup_charset
 
 // Logo Header and Footer-----------------------------------------------------------------------]
 // ---------------------------------------------------------------------------------------------]
 	ldx #40													// Make sure the text color is set to white
 	lda #$01
 !loop:
-	sta $D800, x
+	sta C_COLOUR_RAM, x
 	sta $DA7F, x
 	dex
+	bne !loop-
+
+	ldx #$00												// Make sure the scroller is white
+!loop:
+	//sta C_COLOUR_RAM + $200, x
+	sta C_COLOUR_RAM + $2e8, x
+	inx
 	bne !loop-
 
 	lda #%00000001
@@ -202,12 +216,6 @@ sync_intro:
 	inx
 	jmp !loop-
 !over:
-
-//	sta $D800
-//	lda #001                        // test characters
-//	sta $400                        // used to verify character positions before / after fld
-//	sta $770
-//	sta $680
 
 	jmp hook_init_frame_fld
 
@@ -274,7 +282,7 @@ bitmap_top_fld_done:
 	and #007
 	adc #$38												// %00111000
 	sta REG_SCREENCTL_1
-	lda #%00111000									// $2000-$3fff char mem, $0c00-$0fff screen mem
+	lda #%00111000									// $2000-$3fff bitmap mem, $0c00-$0fff screen mem
 	sta REG_MEMSETUP
 	lda #$d8                        // switch multi colour mode on
 	sta REG_SCREENCTL_2
@@ -296,15 +304,26 @@ hook_sprite_multi:
 
 sprite_multi:
 	inc REG_INTFLAG                 // acknowledge interrupt
-	ldy #122				// Set Y position of sprite
-  sty $D001				// Set y for sprite 0
-  sty $D003				// Set y for sprite 1
-  sty $D005				// Set y for sprite 2
-  sty $D007				// Set y for sprite 3
-  sty $D009				// Set y for sprite 4
-  sty $D00B				// Set y for sprite 5
-  sty $D00D				// Set y for sprite 6
-  sty $D00F				// Set y for sprite 6
+	ldy #122												// Set Y position of sprite
+  sty $D001												// Set y for sprite 0
+  sty $D003												// Set y for sprite 1
+  sty $D005												// Set y for sprite 2
+  sty $D007												// Set y for sprite 3
+  sty $D009												// Set y for sprite 4
+  sty $D00B												// Set y for sprite 5
+  sty $D00D												// Set y for sprite 6
+  sty $D00F												// Set y for sprite 6
+
+.if (DEBUG==1) {
+	lda #$0A
+	sta $d020
+}
+	jsr update_scroller							// Update the scroller state
+.if (DEBUG==1) {
+	lda #$00
+	sta $d020
+}
+
 	jmp hook_bitmap_end
 
 // complete rendering bitmap -------------------------------------------------------------------]
@@ -347,34 +366,72 @@ latch_final_bitmap_line:
 	and #007                        // maintain current vertical scroll bits
 	adc #024
 	sta REG_SCREENCTL_1
-	lda #%00010110								  // $1800-1ffff char mem, $0400-07ff screen mem
+	lda #%00011110								  // $1800-1ffff char mem, $0400-07ff screen mem
 	sta REG_MEMSETUP
 	lda #200                        // multi colour mode off
 	sta REG_SCREENCTL_2
 
 	// Sprites back to top
-	ldy #80				// Set Y position of sprite
-	sty $D001				// Set y for sprite 0
-	sty $D003				// Set y for sprite 1
-	sty $D005				// Set y for sprite 2
-	sty $D007				// Set y for sprite 3
-	sty $D009				// Set y for sprite 4
-	sty $D00B				// Set y for sprite 5
-	sty $D00D				// Set y for sprite 6
-	sty $D00F				// Set y for sprite 6
+	ldy #80													// Set Y position of sprite
+	sty $D001												// Set y for sprite 0
+	sty $D003												// Set y for sprite 1
+	sty $D005												// Set y for sprite 2
+	sty $D007												// Set y for sprite 3
+	sty $D009												// Set y for sprite 4
+	sty $D00B												// Set y for sprite 5
+	sty $D00D												// Set y for sprite 6
+	sty $D00F												// Set y for sprite 6
 
 .if (DEBUG==1) {
 	ldy #$00
 	sty $d020
 }
+	jmp hook_apply_hw_scroll
+
+// apply hardware scroll -------------------------------------------------------------------------------------------]
+// -----------------------------------------------------------------------------------------------------------------]
+hook_apply_hw_scroll:
+	lda #202
+	ldx #<apply_hardware_scroll
+	ldy #>apply_hardware_scroll
+	jmp apply_interrupt
+
+apply_hardware_scroll:
+	lda #$c0                   // 38 column
+scroller_amount:
+	ora #007                   // + hardware scroll Self modifying
+	ldy #202                   // wait first scroller scanline
+wait_scroller_start:
+	cpy REG_RASTERLINE
+	bne wait_scroller_start
+	sta REG_SCREENCTL_2        // apply hardware scroll value
+	jmp hook_reset_hw_scroll
+
+// reset hardware scroll -------------------------------------------------------------------------------------------]
+// -----------------------------------------------------------------------------------------------------------------]
+hook_reset_hw_scroll:
+	lda #244
+	ldx #<reset_hardware_scroll
+	ldy #>reset_hardware_scroll
+	jmp apply_interrupt
+
+reset_hardware_scroll:
+	lda #$c8                   // 40 column mode + no scroll
+	ldy #250                   // wait for final scanline done
+wait_scroller_end:
+	cpy REG_RASTERLINE
+	bne wait_scroller_end
+	sta REG_SCREENCTL_2        // reset scroll & column mode
+
 	jmp hook_update_logo_fld
+
 
 
 // update fld effect ---------------------------------------------------------------------------]
 // ---------------------------------------------------------------------------------------------]
 
 hook_update_logo_fld:
-	lda #250
+	lda #254
 	ldx #<update_logo_fld
 	ldy #>update_logo_fld
 	jmp apply_interrupt
@@ -420,6 +477,7 @@ update_logo_fld_done:
 	sta $d020
 }
 	jmp hook_init_frame_fld
+
 
 // Update the X sprite animation----------------------------------------------------------------]
 // ---------------------------------------------------------------------------------------------]
@@ -494,8 +552,158 @@ update_x_sprite_anim:
 
 	rts
 
+// Setup alternative Character set -------------------------------------------------------------]
+// ---------------------------------------------------------------------------------------------]
+setup_charset:
+	ldx #$00																// relocate character set data
+!loop:
+	lda font_data, x
+	sta C_CHARSET, x
+	lda font_data + $100, x
+	sta C_CHARSET + $100, x
+	lda font_data + $180, x
+	sta C_CHARSET + $180, x
+	inx
+	bne !loop-
 
-// variables -----------------------------------------------------------------------------------]
+	rts
+
+// update & render scroller ----------------------------------------------------------------------------------------]
+// -----------------------------------------------------------------------------------------------------------------]
+
+update_scroller:
+	ldx scroller_amount + 1
+	dex                             // advance hardware scroll
+	dex
+	dex
+	bmi shift_scroller_data         // detect if time to shift screen ram on the scroller
+	stx scroller_amount + 1         // not time to advance scroller, so just update the hardware scroll value
+	rts														  // This frame no update is needed
+
+shift_scroller_data:
+	stx REG_ZERO_FE                 // cache scroll amount for later use
+
+	ldy #000                        // shift screen ram to the left
+scroller_shift_loop:
+	lda C_SCREEN_RAM + $2f9, y      // shift all 4 rows
+	sta C_SCREEN_RAM + $2f8, y
+	lda C_SCREEN_RAM + $321, y
+	sta C_SCREEN_RAM + $320, y
+	lda C_SCREEN_RAM + $349, y
+	sta C_SCREEN_RAM + $348, y
+	lda C_SCREEN_RAM + $371, y
+	sta C_SCREEN_RAM + $370, y
+	iny
+	cpy #039
+	bne scroller_shift_loop
+
+	ldx scroller_char_step + 1      // grab step into rendering current scroller character
+	bpl render_next_scroll_colm     // detect if we need to render a new character, or are still rendering current character (each letter is 4 chars wide)
+
+scroller_message_index:
+	ldx #000                        // time to render a new character, so set up some which character to render and the bit mask
+read_next_scroller_char:
+	lda scroller_message, x         // grab next character to render
+	bpl advance_scroller_index      // detect end of message control character
+	lda scroller_message
+	ldx #001                        // reset index - set to 1 since this update will use first char in message
+	ldy #>scroller_message          // reset scroller message read source
+	sty read_next_scroller_char + 2
+	jmp save_scroller_index
+
+advance_scroller_index:
+	inx                             // advance scroller message index
+	bne save_scroller_index         // detect if reached 256 offset
+	inc read_next_scroller_char + 2 // advance high byte for reading message
+save_scroller_index:
+	stx scroller_message_index + 1
+
+	ldy #>C_CHARSET                 // determine if character is in the low/high section of the charset
+	cmp #031
+	bcc calc_scrollchar_src_low
+	ldy #>C_CHARSET_HIGH
+
+calc_scrollchar_src_low:
+	and #031                        // calculate offset into char set for character bytes
+	asl
+	asl
+	asl
+
+	sty render_scroller_column + 2  // store character high/low pointers for rendering
+	sty render_scroller_column2 + 2
+	sta render_scroller_column + 1
+	sta render_scroller_column2 + 1
+
+	lda #192                        // reset the scroller character mask
+	sta scroller_character_mask + 1
+	lda #003                        // reset step into new character mask
+	sta scroller_char_step + 1
+
+render_next_scroll_colm:
+	clc
+	lda REG_ZERO_FE                 // reset the hardware scroll value
+	adc #008
+	tax
+	stx scroller_amount + 1         // save hardware scroll index
+
+	ldx #000                        // init character byte loop counter
+	stx REG_ZERO_FD                 // reset screen rendering offset and cache on zero page
+render_scroller_column:
+	lda C_CHARSET, x                // load byte from character ram
+scroller_character_mask:
+	and #192                        // apply current mask
+scroller_char_step:
+	ldy #255
+	beq skip_shift_1                // dont shift if we are already masking bits 0 and 1
+shift_scroll_mask_loop1:
+	lsr                             // shift down until bits 0 and 1 are occupied
+	lsr
+	dey
+	bne shift_scroll_mask_loop1
+skip_shift_1:
+	asl                             // multiply by 4 as a look up into our character matrix
+	asl
+	sta REG_ZERO_FE                 // cache on zero page to recall shortly
+
+	inx                             // advance to next byte in character ram
+render_scroller_column2:
+	lda C_CHARSET, x
+	and scroller_character_mask + 1 // apply current mask
+	ldy scroller_char_step + 1
+	beq skip_shift_2                // dont shift if we are already masking bits 0 and 1
+shift_scroll_mask_loop2:
+	lsr                             // shift down until bits 0 and 1 are occupied
+	lsr
+	dey
+	bne shift_scroll_mask_loop2
+
+skip_shift_2:
+	clc                             // calculate characater code to use for this 2x2 block
+	adc REG_ZERO_FE                 // grab offset calculated earlier
+	adc #064                        // add offset to the character matrix
+
+scroller_render_offset:
+	ldy REG_ZERO_FD
+	sta C_SCREEN_RAM + $31f, y      // render character to screen
+	tya
+	adc #040                        // advance rendering offset for next pass of the loop
+	sta REG_ZERO_FD
+
+	inx                             // advance to next byte in character ram
+	cpx #008                        // detect if entire column now rendered
+	bne render_scroller_column
+
+	dec scroller_char_step + 1      // advance scroller character step
+	lda scroller_character_mask + 1 // advance scroller character mask for next update
+	lsr
+	lsr
+	sta scroller_character_mask + 1
+
+	rts
+
+
+
+// Variables -----------------------------------------------------------------------------------]
 // ---------------------------------------------------------------------------------------------]
 
 logo_bounce_heights:
@@ -509,11 +717,18 @@ back_run_pos:
 temp_run_pos:
 	.byte 000
 header_text:
-	.text "=-=-=-=-=> deFEEST logo below <=-=-=-=-="
+	.text "=-=-=-=-=> defeest logo below <=-=-=-=-="
 	.byte 0
 footer_text:
-	.text "=-=-=-=-=> deFEEST logo above <=-=-=-=-="
+	.text "=-=-=-=-=> defeest logo above <=-=-=-=-="
 	.byte 0
+scroller_message:
+	.text "floating flamingo cup holders, game of beer pong, champagne bottle pool float, a giant f#@%ing tote, a schnoodle, waterproof phone case. "
+	.text "greetings to revspace, frack, lag, hack42, bitlair, nurdspace, tkkrlab and ijhack. "
+	.text "also geraffel, mononoke, netknurft and last but not least je moeder. "
+	.text "this demo was made possible by reichsoverheid.nl and is powered by hoe.re. "
+	.text "daar in dat kleine cafe op de 5e daar zijn de mensen gelijk en tervree etv! ..... "
+	.byte $ff		// EOF
 
 // Data
 checkboard_sprite:
@@ -550,3 +765,49 @@ bounce:
   .print x
   .byte x
 }
+
+// Font data alternative font
+font_data:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00, $7c, $c6, $de, $c6, $c6, $c6, $c6, $00
+	.byte $fc, $c6, $dc, $c6, $c6, $c6, $fc, $00, $7c, $c6, $c0, $c0, $c0, $c6, $7c, $00
+	.byte $fc, $c6, $c6, $c6, $c6, $c6, $fc, $00, $7c, $c6, $f8, $c0, $c0, $c6, $7c, $00
+	.byte $7c, $c6, $f8, $c0, $c0, $c0, $c0, $c0, $7c, $c0, $ce, $c6, $c6, $c6, $7c, $00
+	.byte $c6, $c6, $de, $c6, $c6, $c6, $c6, $c0, $18, $18, $18, $18, $18, $18, $18, $00
+	.byte $06, $06, $06, $06, $06, $06, $c6, $7c, $c6, $c6, $dc, $c6, $c6, $c6, $c6, $00
+	.byte $c0, $c0, $c0, $c0, $c0, $c6, $7c, $00, $6c, $fe, $c6, $c6, $c6, $c6, $c6, $00
+	.byte $7c, $c6, $c6, $c6, $c6, $c6, $c6, $00, $7c, $c6, $c6, $c6, $c6, $c6, $7c, $00
+	.byte $fc, $c6, $dc, $c0, $c0, $c0, $c0, $c0, $7c, $c6, $c6, $c6, $c6, $c6, $7c, $06
+	.byte $fc, $c6, $dc, $c6, $c6, $c6, $c6, $00, $7c, $c0, $fc, $06, $06, $c6, $7c, $00
+	.byte $7e, $18, $18, $18, $18, $18, $18, $00, $c6, $c6, $c6, $c6, $c6, $c6, $7c, $00
+	.byte $c6, $c6, $c6, $c6, $c6, $6c, $38, $00, $c6, $c6, $c6, $c6, $c6, $fe, $6c, $00
+	.byte $c6, $c6, $7c, $1c, $c6, $c6, $c6, $00, $c6, $c6, $7c, $38, $38, $38, $38, $00
+	.byte $fe, $0c, $18, $30, $60, $c0, $fe, $00, $3c, $30, $30, $30, $30, $30, $3c, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00, $3c, $0c, $0c, $0c, $0c, $0c, $3c, $00
+	.byte $00, $18, $3c, $7e, $18, $18, $18, $18, $00, $10, $30, $7f, $7f, $30, $10, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00, $18, $18, $18, $18, $00, $00, $18, $00
+	.byte $66, $66, $66, $00, $00, $00, $00, $00, $66, $66, $ff, $66, $ff, $66, $66, $00
+	.byte $18, $3e, $60, $3c, $06, $7c, $18, $00, $62, $66, $0c, $18, $30, $66, $46, $00
+	.byte $3c, $66, $3c, $38, $67, $66, $3f, $00, $06, $0c, $18, $00, $00, $00, $00, $00
+	.byte $0c, $18, $30, $30, $30, $18, $0c, $00, $30, $18, $0c, $0c, $0c, $18, $30, $00
+	.byte $00, $66, $3c, $18, $3c, $66, $00, $00, $00, $18, $18, $7e, $18, $18, $00, $00
+	.byte $00, $00, $00, $00, $00, $18, $18, $30, $00, $00, $00, $7e, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $30, $30, $00, $00, $03, $06, $0c, $18, $30, $60, $00
+	.byte $7c, $c6, $c6, $c6, $c6, $c6, $7c, $00, $38, $18, $18, $18, $18, $18, $18, $00
+	.byte $7c, $06, $7c, $c0, $c0, $c6, $7c, $00, $7c, $c6, $3c, $06, $06, $c6, $7c, $00
+	.byte $c6, $c6, $7e, $06, $06, $06, $06, $00, $fe, $c0, $fc, $06, $06, $c6, $7c, $00
+	.byte $7c, $c0, $dc, $c6, $c6, $c6, $7c, $00, $7e, $c6, $06, $06, $06, $06, $06, $00
+	.byte $7c, $c6, $7c, $c6, $c6, $c6, $7c, $00, $7e, $c6, $76, $06, $06, $06, $06, $00
+	.byte $00, $30, $30, $00, $30, $30, $00, $00, $00, $00, $18, $00, $00, $18, $18, $30
+	.byte $0e, $18, $30, $60, $30, $18, $0e, $00, $00, $00, $3c, $78, $00, $3c, $78, $00
+	.byte $70, $18, $0c, $06, $0c, $18, $70, $00, $3c, $66, $06, $0c, $18, $00, $18, $00
+
+	//; scroller characters (16 chars)
+	.byte $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $0e, $0e, $0e, $00
+	.byte $00, $00, $00, $00, $e0, $e0, $e0, $00, $00, $00, $00, $00, $ee, $ee, $ee, $00
+	.byte $0e, $0e, $0e, $00, $00, $00, $00, $00, $0e, $0e, $0e, $00, $0e, $0e, $0e, $00
+	.byte $0e, $0e, $0e, $00, $e0, $e0, $e0, $00, $0e, $0e, $0e, $00, $ee, $ee, $ee, $00
+	.byte $e0, $e0, $e0, $00, $00, $00, $00, $00, $e0, $e0, $e0, $00, $0e, $0e, $0e, $00
+	.byte $e0, $e0, $e0, $00, $e0, $e0, $e0, $00, $e0, $e0, $e0, $00, $ee, $ee, $ee, $00
+	.byte $ee, $ee, $ee, $00, $00, $00, $00, $00, $ee, $ee, $ee, $00, $0e, $0e, $0e, $00
+	.byte $ee, $ee, $ee, $00, $e0, $e0, $e0, $00, $ee, $ee, $ee, $00, $ee, $ee, $ee, $00
+
